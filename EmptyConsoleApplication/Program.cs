9de87c9e503a8d66e84a106449a8336f393936e2
Seda Yaman <IS97079@gm.isbank.com.tr>
2017-11-22 15:14:50 +0300

    about me'

--- EmptyConsoleApplication/Program.cs
+++ EmptyConsoleApplication/Program.cs
@@ -10,6 +10,24 @@ namespace EmptyConsoleApplication
     {
         static void Main(string[] args)
         {
+          
+
+            string fullName ="Seda Yaman";
+            short  experience= 8;
+            string department ="Uygulama Destek Uzman Yardımcısı";
+            string programmingLanguage="C#,JAVA,.NET,HTML";
+            string experation="Yeni bilgiler ile kendimi geliştirmek";
+
+            Console.WriteLine("İsim:"+fullName);
+            Console.WriteLine("Tecrübe:"+experience+"ay");
+            Console.WriteLine("Bölüm:"+department);
+            Console.WriteLine("Diller:"+programmingLanguage);
+            Console.WriteLine("Beklenti:"+experation);
+           
+            Console.ReadLine();
+
+          
+
         }
     }
 }
