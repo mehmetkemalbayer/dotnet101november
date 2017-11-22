511e0be0627dfd71380deb77d73d00e9f4d5d14b
Serdar Uz <serdar.uz@softtech.com.tr>
2017-11-22 15:04:46 +0300

    about me

--- EmptyConsoleApplication/Program.cs
+++ EmptyConsoleApplication/Program.cs
@@ -10,6 +10,17 @@ namespace EmptyConsoleApplication
     {
         static void Main(string[] args)
         {
+            string fullName = "Serdar Uz";
+            short experience = 11;
+            string department = "Süreç ve Ölçme";
+            string programmingLanguages = "excel vba, rexx, jcl";
+            string expectation = "C# esaslarını öğrenerek iş için bir temel oluşturmak";
+            Console.WriteLine("İsim: " + fullName);
+            Console.WriteLine("Deneyim " + experience);
+            Console.WriteLine("Bölüm: " + department);
+            Console.WriteLine("Diller: " + programmingLanguages);
+            Console.WriteLine("Beklenti: " + expectation);
+            Console.ReadLine();
         }
     }
 }
