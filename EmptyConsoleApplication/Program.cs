465d37335f74175363445a43fbdfe7b6d0da8f76
Selçuk Er <IS97133@gm.isbank.com.tr>
2017-11-22 15:16:15 +0300

    about me

--- EmptyConsoleApplication/Program.cs
+++ EmptyConsoleApplication/Program.cs
@@ -10,6 +10,18 @@ namespace EmptyConsoleApplication
     {
         static void Main(string[] args)
         {
-        }
+            string fullName =  "Selcuk ER";
+            short experience =  5;
+            string department = "Uygulama Destek";
+            string programingLanguage = "PLSQL,MicrosoSQL,Oracle";
+            string expection =  "Kodlama konusunda iyi bir noktaya gelebilmek";
+            Console.WriteLine("İsim" + fullName);
+            Console.WriteLine("Tecrübe"+ experience);
+            Console.WriteLine("Bölüm"+ department);
+            Console.WriteLine("Programlama Dili"+ programingLanguage);
+            Console.WriteLine("Beklenti" + expection);
+            Console.ReadLine();
+
+    }
     }
 }
