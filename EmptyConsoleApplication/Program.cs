06a35e9d1ce9cb8cbac5b3827c47d2eb994cf39c
Seren Hacaloğlu <IS96457@gm.isbank.com.tr>
2017-11-22 14:59:56 +0300

    about me

--- EmptyConsoleApplication/Program.cs
+++ EmptyConsoleApplication/Program.cs
@@ -10,6 +10,18 @@ namespace EmptyConsoleApplication
     {
         static void Main(string[] args)
         {
+            string fullName = "Seren Hacaloğlu";
+            short experience = 6;
+            string department = "Sermaye Piyasaları Uygulamaları";
+            string programmingLanguage = "Cobol, C#";
+            string expectation = "Herkes bu işi yapabileceğini düşünerek ayrılsın.";
+            Console.WriteLine("İsim: " + fullName);
+            Console.WriteLine("Tecrübe: " + experience);
+            Console.WriteLine("Bölüm: " + department);
+            Console.WriteLine("Diller: " + programmingLanguage);
+            Console.WriteLine("Beklenti: " + expectation);
+            Console.ReadLine();
+//            System.Threading.Thread.Sleep(10000);
         }
     }
 }
