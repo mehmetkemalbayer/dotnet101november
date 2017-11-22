4c6941a46130f47309c3fd9f7757052401defc1a
mehmetkemalbayer <kemal.bayer@gmail.com>
2017-11-22 14:56:25 +0300

    about me

--- EmptyConsoleApplication/Program.cs
+++ EmptyConsoleApplication/Program.cs
@@ -10,6 +10,17 @@ namespace EmptyConsoleApplication
     {
         static void Main(string[] args)
         {
+            string fullName = "Mehmet Kemal Bayer";
+            short experience = 10;
+            string department = "Inovasyon";
+            string programmingLanguages = ".net, cobol, node.js";
+            string expectation = "Herkes bu işi yapabileceğini düşünerek ayrılsın.";
+            Console.WriteLine("İsim: " + fullName);
+            Console.WriteLine("Tecrübe: " + experience);
+            Console.WriteLine("Bölüm: " + department);
+            Console.WriteLine("Diller: " + programmingLanguages);
+            Console.WriteLine("Beklenti: " + expectation);
+            Console.ReadLine();
         }
     }
 }
