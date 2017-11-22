fe29f84b4e7db7820aa5277555f3afc2ca79dbe6
Yavuz Gençoğlu <IS97131@gm.isbank.com.tr>
2017-11-22 15:02:42 +0300

    about me file

--- EmptyConsoleApplication/Program.cs
+++ EmptyConsoleApplication/Program.cs
@@ -10,6 +10,19 @@ namespace EmptyConsoleApplication
     {
         static void Main(string[] args)
         {
+
+            string fullName = "Yavuz Gençoğlu";
+            short experience = 5;
+            string department = "GullsEye";
+            string programingLanguages = ".net,EBA,MsSQL";
+            string expectetion = "Bu işi yapabileceğimi düşünerek ayrılacağım";
+
+            Console.WriteLine("İsim : " + fullName);
+            Console.WriteLine("Tecrübe : " + experience);
+            Console.WriteLine("Departman : " + department);
+            Console.WriteLine("Diller : " + programingLanguages);
+            Console.WriteLine("Beklenti : " + expectetion);
+
         }
     }
 }
