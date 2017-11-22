72cbc4969c23f6f209fd53c3382429d7d1945c64
Gözde Oğuz <IS96977@gm.isbank.com.tr>
2017-11-22 15:00:52 +0300

    about me

--- EmptyConsoleApplication/Program.cs
+++ EmptyConsoleApplication/Program.cs
@@ -10,6 +10,17 @@ namespace EmptyConsoleApplication
     {
         static void Main(string[] args)
         {
+            string fullName  = "Gözde Oğuz";
+            short experience = 2;
+            string department = "Taksitli Krediler ve Ek Hesap Uygulamaları";
+            string programmingLanguage = " .net, cobol";
+            string expectation = "sadece kodlamayı değil nasıl kullanacağımı öğrenmek";
+            Console.WriteLine( "İsim :" + fullName);
+            Console.WriteLine( "Tecrübe :" + experience);
+            Console.WriteLine( "Bölüm :" + department);
+            Console.WriteLine( "Prog. Dilleri :" + programmingLanguage);
+            Console.WriteLine( "Beklenti :" + expectation);
+            Console.ReadLine();
         }
     }
 }
