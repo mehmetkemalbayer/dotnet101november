774293f5c799c20e6b254e0be50f1d53cb72df66
ISBANK\EK57019 <emre.kalyon@softtech.com.tr>
2017-11-22 14:57:41 +0300

    about me

--- EmptyConsoleApplication/Program.cs
+++ EmptyConsoleApplication/Program.cs
@@ -10,6 +10,17 @@ namespace EmptyConsoleApplication
     {
         static void Main(string[] args)
         {
+            string fullName = "Emre KALYON";
+            short experience = 10;
+            string programmingLanguages = ".net, cobol";
+            string expectations = ".NET yazabileceğimi düşünerek ayrılmak";
+            string department = "Resmi ve Özel Ödemeler Müdürlüğü";
+            Console.WriteLine("İsim: " + fullName);
+            Console.WriteLine("Tecrübe: " + experience);
+            Console.WriteLine("Bölüm: " + department);
+            Console.WriteLine("Kullandığı Diller: " + programmingLanguages);
+            Console.WriteLine("Bekletiler: " + expectations);
+            Console.ReadLine();
         }
     }
 }
