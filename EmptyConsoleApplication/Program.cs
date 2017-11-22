a550035a295c3c93ef033f4eba0a41d8b2b6c2dc
Cihan Omur <IS97146@gm.isbank.com.tr>
2017-11-22 15:14:29 +0300

    degisikilik

--- EmptyConsoleApplication/Program.cs
+++ EmptyConsoleApplication/Program.cs
@@ -10,6 +10,18 @@ namespace EmptyConsoleApplication
     {
         static void Main(string[] args)
         {
+            string fullName = " Cihan Omur";
+            string experience = "5 ay";
+            string department = "Resmi ve Özel Kurum Ödm. Uyg.";
+            string programmingLanguages = "Cobol, Java";
+            string expectetion = "C# la ilgili birşeyler yazabilmek";
+                Console.WriteLine( "İsim: " + fullName );
+                Console.WriteLine("Tecrübe: " + experience);
+                Console.WriteLine("Departman: " + department);
+                Console.WriteLine("Programlama Dilleri: " + programmingLanguages);
+            Console.WriteLine("Beklentiler: " + expectetion);         
+                Console.ReadLine();
+                           
         }
     }
 }
