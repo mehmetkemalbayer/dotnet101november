4e5651c83f317541e31f3386fceeec3b387a44b2
Nilufer Ural <nilufer.erdem@softtech.com.tr>
2017-11-22 15:02:36 +0300

    add : personal info

--- EmptyConsoleApplication/Program.cs
+++ EmptyConsoleApplication/Program.cs
@@ -10,6 +10,16 @@ namespace EmptyConsoleApplication
     {
         static void Main(string[] args)
         {
+            String name = "nilüfer";
+            int experience = 12;
+            String department = "mimari yönetim";
+            String expectation = "huzur";
+
+            Console.WriteLine("isim : " + name);
+            Console.WriteLine("tecrübe : " + experience);
+            Console.WriteLine("bölüm : " + department);
+            Console.WriteLine("beklenti : " + expectation);
+            Console.Read();
         }
     }
 }
