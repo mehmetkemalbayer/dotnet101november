6376d08803b60dea87c729754fd8aa4bfc202eac
Ceyhun Taşdemir <IS97138@gm.isbank.com.tr>
2017-11-22 15:31:25 +0300

    ben

--- EmptyConsoleApplication/Program.cs
+++ EmptyConsoleApplication/Program.cs
@@ -10,6 +10,19 @@ namespace EmptyConsoleApplication
     {
         static void Main(string[] args)
         {
+            string fullname = "CeyhunTasdemir";
+            string experience = "6ay";
+            string department = "Uygulama Destek";
+            string Languge = "Cobol";
+            string expectation = "C# mantığını kavramak";
+            Console.WriteLine("isim:"+ fullname);
+            Console.WriteLine("tecrübe:"+ experience);
+            Console.WriteLine("bölüm:"+ department);
+            Console.WriteLine("dil:"+Languge );
+            Console.WriteLine("beklenti:"+ expectation);
+            Console.Read();
+
         }
+
     }
 }
