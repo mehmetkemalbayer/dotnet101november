fc8a79b4f459cdd2afb540a0f3e4d893411264f6
Mehmet Zeki Seyhan <IS97085@gm.isbank.com.tr>
2017-11-22 15:29:01 +0300

    about me

--- EmptyConsoleApplication/Program.cs
+++ EmptyConsoleApplication/Program.cs
@@ -13,9 +13,11 @@ namespace EmptyConsoleApplication
 			string fullname = "Mehmet Zeki Seyhan";
 			string experience = "8 ay";
 			string department = "Uygulama Destek - Sermaye + Hazine";
+			string expectation = "bu işi yapabileceğimi düşünerek ayrılacağım..."; 
 			Console.WriteLine("İsim: " + fullname );
 			Console.WriteLine("Deneyim: " + experience);
 			Console.WriteLine("Bölüm: " + department);
+			Console.WriteLine("Beklenti: " + expectation);
 			Console.ReadLine();
 
         }
