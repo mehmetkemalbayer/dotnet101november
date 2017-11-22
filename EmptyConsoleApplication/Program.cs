51e31c44ce7b0755ed99c642e49f6eae7e67dfca
ISBANK\IS97088 <oyku.bilen@softtech.com.tr>
2017-11-22 15:02:22 +0300

    about me

--- EmptyConsoleApplication/Program.cs
+++ EmptyConsoleApplication/Program.cs
@@ -10,6 +10,24 @@ namespace EmptyConsoleApplication
     {
         static void Main(string[] args)
         {
+
+
+            string fullName = "Öykü BİLEN";
+            short experience = 8;
+            string department = "Uygulama Destek";
+            string programmingLanguages = "ASP.NET MVC,C#,C,MATLAB,JAVA";
+            string expectation = "Maximum  ";
+
+            Console.WriteLine("İsim :" + fullName);
+            Console.WriteLine("Deneyim :" + experience + "ay");
+            Console.WriteLine("Programlama dilleri :" + programmingLanguages);
+            Console.WriteLine("Beklenti :" + expectation);
+
+           
+            Console.ReadLine();
+
+          
         }
+        
     }
 }
