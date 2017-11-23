b02cf0d5d2e767845a6e12ad0ec7e05dc0d67356
ISBANK\EK57019 <emre.kalyon@softtech.com.tr>
2017-11-23 12:31:53 +0300

    user

--- EmptyConsoleApplication/Program.cs
+++ EmptyConsoleApplication/Program.cs
@@ -4,32 +4,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApplication2
+namespace EmptyConsoleApplication
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Tutar Giriniz: ");
-            string girilenTutar = Console.ReadLine();
-            var convertDecimal = Convert.ToDecimal(girilenTutar);
-            Console.WriteLine(girilenTutar + " girdiniz.");
+            User firstUser = new User(111);
+            firstUser.WriteInfo();
+            User secondUser = new User();
+            secondUser.WriteInfo();
 
 
-            Console.WriteLine("Yıllık Faizi Giriniz: ");
-            string girilenFaiz = Console.ReadLine();
-            var convertInterest = Convert.ToDecimal(girilenFaiz);
-            Console.WriteLine(convertInterest + " faiz oranıyla hesaplama yapılacak.");
 
-            Console.WriteLine("Kaç Yıl Vade ile İşlem Yapılacağını Giriniz: ");
-            string girilenYil = Console.ReadLine();
-            var vadeYil = Convert.ToDecimal(girilenYil);
-            Console.WriteLine(vadeYil + " yıl için faiz hesaplanacak.");
-
-            Console.ReadLine();
-            decimal futureValue = (convertInterest * convertDecimal / 100) + convertDecimal;
-            Console.WriteLine("Dönem Sonunda Ödenecek Tutar: " + futureValue);
-            Console.ReadLine();
         }
     }
 }
---
+++ EmptyConsoleApplication/User.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmptyConsoleApplication
+{
+    class User
+    {
+        public User(long CitizenNumber)
+        {
+            this.CitizenNumber = CitizenNumber;
+        }
+        public User()
+        {
+            Name = "Default";
+            Age = 0;
+            PhoneNumber = "555555555";
+            CitizenNumber = 00000000000;
+            Gender = '-';
+        }
+
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Fullname { get; set; }
+        public short Age { get; set; }
+        public string PhoneNumber { get; set; }
+        public long CitizenNumber { get; set; }
+        public char Gender { get; set; }
+
+        void GenerateFullName()
+        {
+            Fullname = Name + Surname;
+        }
+        public void WriteInfo()
+        {
+            
+            Console.WriteLine("Ad Soyad: " + Fullname + "\nYaş: " + Age + "\nTelefon Numarası: " + PhoneNumber + "\nTCKN: " + CitizenNumber + "\nCinsiyet: " + Gender );
+            Console.ReadLine();
+        }
+
+
+    }
+}
