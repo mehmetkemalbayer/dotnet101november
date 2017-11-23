5dd8905c7c8219e3a6f0b686ab4e918aae10e338
Nilufer Ural <nilufer.erdem@softtech.com.tr>
2017-11-23 15:12:06 +0300

    new: Calculate works

--- EmptyConsoleApplication/Program.cs
+++ EmptyConsoleApplication/Program.cs
@@ -7,14 +7,13 @@ namespace EmptyConsoleApplication
     {
         static void Main(string[] args)
         {
-            User firstUser = new User(5);
-            Console.WriteLine("birinci kullanıcı : ");
-            firstUser.WriteInfo();
-            User secondUser = new User();
-            Console.WriteLine("\nikinci kullanıcı : ");
-            secondUser.WriteInfo();
+            Console.WriteLine("işlem için sayı giriniz:");
+            int sayi1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("işlem için ikini sayıyı giriniz:");
+            int sayi2 = Convert.ToInt32(Console.ReadLine());
+            SimpleCalculator sCalc = new SimpleCalculator(sayi1, sayi2);
+            Console.WriteLine("toplam={0}", sCalc.Sum());
             Console.Read();
-
         }
     }
 }
---
+++ EmptyConsoleApplication/SimpleCalculator.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+
+namespace EmptyConsoleApplication
+{
+    class SimpleCalculator
+    {
+        int a;
+        int b;
+
+        public SimpleCalculator(int a, int b)
+        {
+            this.a = a;
+            this.b = b;
+        }
+
+        public int Sum()
+        {
+            return a + b;
+        }
+
+        public int Substract()
+        {
+            return a - b;
+        }
+
+        public int Multiple()
+        {
+            return a * b;
+        }
+
+        public int Divide()
+        {
+            return a / b;
+        }
+    }
+}
