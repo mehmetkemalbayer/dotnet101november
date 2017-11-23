a347f339b65278800a8600f4c7b8fd41004b06b8
mehmetkemalbayer <kemal.bayer@gmail.com>
2017-11-23 12:11:47 +0300

    user example

--- EmptyConsoleApplication/Program.cs
+++ EmptyConsoleApplication/Program.cs
@@ -10,17 +10,18 @@ namespace EmptyConsoleApplication
     {
         static void Main(string[] args)
         {
-            string fullName = "Mehmet Kemal Bayer";
-            short experience = 10;
-            string department = "Inovasyon";
-            string programmingLanguages = ".net, cobol, node.js";
-            string expectation = "Herkes bu işi yapabileceğini düşünerek ayrılsın.";
-            Console.WriteLine("İsim: " + fullName);
-            Console.WriteLine("Tecrübe: " + experience);
-            Console.WriteLine("Bölüm: " + department);
-            Console.WriteLine("Diller: " + programmingLanguages);
-            Console.WriteLine("Beklenti: " + expectation);
-
+            User firstUser = new User(5);
+            firstUser.WriteInfo();
+            User secondUser = new User();
+            secondUser.WriteInfo();
+            /*
+             * Name: "Default"
+             * Age: 0
+             * PhoneNumber: 5555555555
+             * CitizenNumber: 00000000000
+             * Gender: "-"
+             * 
+            */
         }
     }
 }
---
+++ EmptyConsoleApplication/User.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmptyConsoleApplication
+{
+    class User
+    {
+        public User(long CitizenNumber)
+        {
+            this.CitizenNumber = CitizenNumber;
+        }
+        public User()
+        {
+
+        }
+        public string Name { get; set; }
+        public short Age { get; set; }
+        public string PhoneNumber { get; set; }
+        public long CitizenNumber { get; set; }
+        public char Gender { get; set; }
+
+        public void WriteInfo()
+        {
+            Console.WriteLine("Name: " + Name + "\nAge: " + 
+                                Age + "\nPhoneNumber: " + 
+                                PhoneNumber + "\nCitizenNumber: "
+                                + CitizenNumber + "\nGender: " 
+                                + Gender);
+        }
+    }
+}
