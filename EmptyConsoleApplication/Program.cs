e000005eeddb16f85b91af29d9dafc2817f7b1fe
Fatma Gül Bulut <gul.bulut@softtech.isbank.com.tr>
2017-11-23 11:27:53 +0300

    faiz

--- EmptyConsoleApplication/Program.cs
+++ EmptyConsoleApplication/Program.cs
@@ -9,19 +9,39 @@ namespace EmptyConsoleApplication
     class Program
     {
         static void Main(string[] args)
-        {
-            string fullname = ("Gül Bulut");
-            short experience = 2;
-            string department = ("Süreç ve Ölçme");
-            string programminglanguages = ("C");
-            string expectation = ("Herkes bu işi yapabileceğini düşünerek ayrılsın");
-            Console.WriteLine("İsim : " + fullname);
-            Console.WriteLine("Tecrübe : " + experience );
-            Console.WriteLine("Bölüm : " + department);
-            Console.WriteLine("Diller : " + programminglanguages);
-            Console.WriteLine("Beklenti : " + expectation);
-            Console.ReadLine();
+        {            
+
+            Console.WriteLine("Tutar Giriniz : ");
+            string apara = Console.ReadLine();
             
+            Console.WriteLine("Yıl Giriniz : ");
+            string ayıl = Console.ReadLine();
+                        
+            Console.WriteLine("Faiz Giriniz : ");
+            string afaiz = Console.ReadLine();
+
+            decimal para = Convert.ToDecimal(apara);
+            decimal faiz = Convert.ToDecimal(afaiz);
+            int yıl = Convert.ToInt16(ayıl);
+
+
+
+            decimal hesaplama = (para * faiz * yıl) / 100;
+
+            Console.WriteLine(para);
+            Console.WriteLine(ayıl);
+            Console.WriteLine(faiz);
+            Console.WriteLine(hesaplama);
+
+            Console.ReadLine();
+
+
+            
+
+
+
+
         }
+        
     }
 }
