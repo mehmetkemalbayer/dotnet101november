b695667769f48885831570d16ed1461e93588233
Yavuz Gençoğlu <IS97131@gm.isbank.com.tr>
2017-11-23 12:36:09 +0300

    about new me

--- EmptyConsoleApplication/Program.cs
+++ EmptyConsoleApplication/Program.cs
@@ -11,17 +11,11 @@ namespace EmptyConsoleApplication
         static void Main(string[] args)
         {
 
-            string fullName = "Yavuz Gençoğlu";
-            short experience = 5;
-            string department = "GullsEye";
-            string programingLanguages = ".net,EBA,MsSQL";
-            string expectetion = "Bu işi yapabileceğimi düşünerek ayrılacağım";
-
-            Console.WriteLine("İsim : " + fullName);
-            Console.WriteLine("Tecrübe : " + experience);
-            Console.WriteLine("Departman : " + department);
-            Console.WriteLine("Diller : " + programingLanguages);
-            Console.WriteLine("Beklenti : " + expectetion);
+            User firstUser = new User(5);
+            firstUser.WiteInfo();
+            
+            User SecondUser = new User();
+            SecondUser.WiteInfo1();
 
         }
     }
---
+++ EmptyConsoleApplication/User.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmptyConsoleApplication
+{
+     class User
+    {
+        public User()
+        {
+            Name = "Default";
+            Age = 0;
+            PhoneNumber = "55555555";
+            CitizenNumber = 0;
+            Gender = '-';
+        }
+
+        public User(long CitizenNumber)
+        {
+            this.CitizenNumber = CitizenNumber;
+        }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Fullname { get; set; }
+        public short Age { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public long CitizenNumber { get; set; }
+
+        public char Gender { get; set; }
+
+        public void WiteInfo()
+
+        {
+
+            Console.WriteLine("Name : " + Name + "\n Age : " + Age + "\n PhoneNumber : " + PhoneNumber + "\n CitizenNumber : " + CitizenNumber + "\nGender : " + Gender);
+
+        }
+
+        public void WiteInfo1()
+
+        {
+
+            Console.WriteLine("Name : " + Name + "\n Age : " + Age + "\n PhoneNumber : " + PhoneNumber + "\n CitizenNumber : " + CitizenNumber + "\nGender : " + Gender);
+
+        }
+
+    }
+
+
+    }
