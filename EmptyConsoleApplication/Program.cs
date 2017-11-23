fd5a96ddf04062b113806ffd94fc462d67de130b
Seren Hacaloğlu <IS96457@gm.isbank.com.tr>
2017-11-23 11:02:08 +0300

    faiz oranı bulmaca

--- EmptyConsoleApplication/Program.cs
+++ EmptyConsoleApplication/Program.cs
@@ -8,20 +8,36 @@ namespace EmptyConsoleApplication
 {
     class Program
     {
+       
         static void Main(string[] args)
         {
-            string fullName = "Seren Hacaloğlu";
-            short experience = 6;
-            string department = "Sermaye Piyasaları Uygulamaları";
-            string programmingLanguage = "Cobol, C#";
-            string expectation = "Herkes bu işi yapabileceğini düşünerek ayrılsın.";
-            Console.WriteLine("İsim: " + fullName);
-            Console.WriteLine("Tecrübe: " + experience);
-            Console.WriteLine("Bölüm: " + department);
-            Console.WriteLine("Diller: " + programmingLanguage);
-            Console.WriteLine("Beklenti: " + expectation);
+            //int sum = 0;
+            //for (int i = 0; i < 1001; i++)
+            //{
+            //    sum += i;
+            //}
+            //Console.WriteLine(sum);
+            //Console.ReadLine();
+
+            //string input = Console.ReadLine();
+            //Console.WriteLine(input + "girdiniz.");
+
+            Console.WriteLine("Tutar Giriniz.");
+            string stutar = Console.ReadLine();
+            decimal tutar = Convert.ToDecimal(stutar);
+            Console.WriteLine("Yıl Giriniz.");
+            string syil = Console.ReadLine();
+            int yil = Convert.ToInt16(syil);
+            Console.WriteLine("Faiz Oranı Giriniz.");
+            string sfaiz = Console.ReadLine();
+            decimal faiz = Convert.ToDecimal(sfaiz);
+            decimal sonuc = faiz * yil * tutar / 100;
+            Console.WriteLine("Sonuç: " + sonuc);
             Console.ReadLine();
-//            System.Threading.Thread.Sleep(10000);
         }
+
+       
+
+        
     }
 }
