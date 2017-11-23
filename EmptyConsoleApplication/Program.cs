3d879d115862537227462b4328c029ced830cbda
Seren Hacaloğlu <IS96457@gm.isbank.com.tr>
2017-11-23 12:39:10 +0300

    class/accessor/constructor

--- EmptyConsoleApplication/Program.cs
+++ EmptyConsoleApplication/Program.cs
@@ -11,28 +11,12 @@ namespace EmptyConsoleApplication
        
         static void Main(string[] args)
         {
-            //int sum = 0;
-            //for (int i = 0; i < 1001; i++)
-            //{
-            //    sum += i;
-            //}
-            //Console.WriteLine(sum);
-            //Console.ReadLine();
 
-            //string input = Console.ReadLine();
-            //Console.WriteLine(input + "girdiniz.");
-
-            Console.WriteLine("Tutar Giriniz.");
-            string stutar = Console.ReadLine();
-            decimal tutar = Convert.ToDecimal(stutar);
-            Console.WriteLine("Yıl Giriniz.");
-            string syil = Console.ReadLine();
-            int yil = Convert.ToInt16(syil);
-            Console.WriteLine("Faiz Oranı Giriniz.");
-            string sfaiz = Console.ReadLine();
-            decimal faiz = Convert.ToDecimal(sfaiz);
-            decimal sonuc = faiz * yil * tutar / 100;
-            Console.WriteLine("Sonuç: " + sonuc);
+            //User firstUser = new User(5);
+            //firstUser.WriteInfo();
+            User secondUser = new User();
+            secondUser.WriteInfo();
+          
             Console.ReadLine();
         }
 
---
+++ EmptyConsoleApplication/User.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmptyConsoleApplication
+{
+    class User
+    {
+        public User(long CitizenNumber)
+        {
+            this.CitizenNumber = CitizenNumber;
+        }
+
+        public User()
+        {
+            this.Name = "Default";
+            this.PhoneNumber = "5555555555";
+            this.Gender = Convert.ToChar("-");
+
+        }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public short Age { get; set; }
+        public string PhoneNumber { get; set; }
+        public long CitizenNumber { get; set; }
+        public char Gender { get; set; }
+
+        public void WriteInfo()
+        {
+            Console.WriteLine("Name: " + Name + "\nAge: " + Age + "\nPhoneNumber: " + PhoneNumber + "\nCitizenNumber: " + CitizenNumber + "\nGender: " + Gender);
+ //         Console.WriteLine("Name: {0}\nAge: {1}\nPhoneNumber: {2}\nCitizenNumber: {3}\nGender: " Name, PhoneNumber, CitizenNumber, Gender);
+        }
+    }
+}
