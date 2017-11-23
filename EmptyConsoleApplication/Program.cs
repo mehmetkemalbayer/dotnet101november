9334bac0e832df88965259768a33833825e7b516
ISBANK\IS97088 <oyku.bilen@softtech.com.tr>
2017-11-23 11:07:57 +0300

    faiz hesaplama

--- EmptyConsoleApplication/Program.cs
+++ EmptyConsoleApplication/Program.cs
@@ -10,24 +10,65 @@ namespace EmptyConsoleApplication
     {
         static void Main(string[] args)
         {
+            //string companyName="Softtech";
+
+            //string fullName = "Öykü BİLEN";
+            //short experience = 8;
+            //string department = "Uygulama Destek";
+            //string programmingLanguages = "ASP.NET MVC,C#,C,MATLAB,JAVA,SQL";
+            //string expectation = "Kurs süresince bilgi birikimi ile kendimi geliştirerek ileriye yönelik bir kazanç sağlamak";
+
+            //WriteName(fullName);
+            //WriteExperience(experience);
+
+            //Console.WriteLine("Programlama dilleri :" + programmingLanguages);
+            //Console.WriteLine("Beklenti :" + expectation);
+
+            //int toplam=0;
+
+            //for (int i = 0; i < 1000; i++)
+            //{
+            // toplam+=i;
+            //}
+            //Console.WriteLine(toplam);
+
+            double faiz;
+            double faizOran;
+            Console.WriteLine("Ana parayı giriniz :");
+            string para=Console.ReadLine();
+            Console.WriteLine("Faiz oranını giriniz");
+            string faizOran1=Console.ReadLine();
+            faizOran=Convert.ToDouble(faizOran1);
+            decimal anapara = Convert.ToDecimal(para);
+            Console.WriteLine("zaman giriniz :");
+            string zaman=Console.ReadLine();
+            int zaman1=Convert.ToInt32(zaman);
+            faiz=((double)(anapara)*zaman1*faizOran)/100;
+
+            Console.WriteLine("faiz oranı :"+faiz);
 
 
-            string fullName = "Öykü BİLEN";
-            short experience = 8;
-            string department = "Uygulama Destek";
-            string programmingLanguages = "ASP.NET MVC,C#,C,MATLAB,JAVA";
-            string expectation = "Maximum  ";
 
-            Console.WriteLine("İsim :" + fullName);
-            Console.WriteLine("Deneyim :" + experience + "ay");
-            Console.WriteLine("Programlama dilleri :" + programmingLanguages);
-            Console.WriteLine("Beklenti :" + expectation);
+
 
            
             Console.ReadLine();
 
           
         }
+
+      static  void WriteName (string name)
+        {
+            Console.WriteLine("İsim :" + name);
+          Console.WriteLine();
+        }
+
+      static  void WriteExperience(short ay)
+        {
+            Console.WriteLine("Deneyim :" + ay + "ay");
+        }
         
     }
+
+    
 }
