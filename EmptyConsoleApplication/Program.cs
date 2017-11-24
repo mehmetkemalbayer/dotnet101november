b715929ed8ebd358831059501b38fa7c684d3b2d
mehmetkemalbayer <kemal.bayer@gmail.com>
2017-11-24 11:48:09 +0300

    error handling

--- EmptyConsoleApplication/Program.cs
+++ EmptyConsoleApplication/Program.cs
@@ -10,18 +10,39 @@ namespace EmptyConsoleApplication
     {
         static void Main(string[] args)
         {
-            User firstUser = new User(5);
-            firstUser.WriteInfo();
-            User secondUser = new User();
-            secondUser.WriteInfo();
-            /*
-             * Name: "Default"
-             * Age: 0
-             * PhoneNumber: 5555555555
-             * CitizenNumber: 00000000000
-             * Gender: "-"
-             * 
-            */
+            Console.WriteLine("Bir sayı giriniz");
+            var sayi1 = Console.ReadLine();
+            Console.WriteLine("Bir sayı daha giriniz");
+            var sayi2 = Console.ReadLine();
+            if (string.IsNullOrEmpty(sayi2))
+            {
+                throw new EmptyStringException();
+            }
+            int number1 = Convert.ToInt32(sayi1);
+            int number2 = Convert.ToInt32(sayi2);
+            Console.WriteLine("Toplam = {0}", number1 + number2);
+            try
+            {
+               
+            }
+            catch (EmptyStringException ex)
+            {
+                Console.WriteLine("Giriş değeri boş bırakılamaz.");
+                Console.ReadLine();
+            }
+            catch (FormatException  ex)
+            {
+                Console.WriteLine("Uyumsuz format");
+                Console.ReadLine();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Girdiğiniz rakam formatı uygun değil.");
+                Console.ReadLine();
+            }
+
+
         }
+
     }
 }
