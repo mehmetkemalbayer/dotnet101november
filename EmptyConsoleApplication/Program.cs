0df18c235d979874d2bc5337448f4be21543743e
mehmetkemalbayer <kemal.bayer@gmail.com>
2017-11-24 12:26:20 +0300

    refactors the code

--- EmptyConsoleApplication/Program.cs
+++ EmptyConsoleApplication/Program.cs
@@ -9,39 +9,67 @@ namespace EmptyConsoleApplication
 
     class Program
     {
-       
+        static int minDeger = 0;
+        static int maxDeger = 100;
+        static int tahmin = 50;
+        static string durum = "r";
         static void Main(string[] args)
         {
-            int minDeger = 0;
-            int maxDeger = 100;
-            int tahmin = 50;
-            string durum = "r";
-            while (durum!="d")
+
+            
+            while (durum != "d")
             {
 
                 Console.WriteLine("tuttuğun sayı " + tahmin + " mi ?");
-                durum= Console.ReadLine();
-
-                if (durum == "k")
+                durum = Console.ReadLine();
+                try
                 {
-                    maxDeger = tahmin;
-                    tahmin = minDeger + (maxDeger - minDeger) / 2; 
-
+                    ValidateInput();
+                    HandleLessThanCase();
+                    HandleGreaterThanCase();
                 }
-                if (durum == "b")
+                catch (Exception ex)
                 {
-                    minDeger = tahmin;
-                    tahmin = minDeger + (maxDeger - minDeger) / 2;
+                    Console.WriteLine(ex.Message);                    
                 }
+                    
                 
-                    
-                }
+                
+                
 
 
-                    
+
 
             }
-            }
+        }
 
-        }        
+        private static void HandleGreaterThanCase()
+        {
+            if (durum == "b" || durum == "B")
+            {
+                minDeger = tahmin;
+                tahmin = minDeger + (maxDeger - minDeger) / 2;
+            }
+        }
+
+        private static void HandleLessThanCase()
+        {
+            if (durum == "k" || durum == "K")
+            {
+                maxDeger = tahmin;
+                tahmin = minDeger + (maxDeger - minDeger) / 2;
+
+            }
+        }
+
+        static public void ValidateInput()
+        {
+            if (durum != "k") throw new Exception("Sadece B, K, b ve k girebilirsiniz");
+            if (durum != "K") throw new Exception("Sadece B, K, b ve k girebilirsiniz");
+            if (durum != "b") throw new Exception("Sadece B, K, b ve k girebilirsiniz");
+            if (durum != "B") throw new Exception("Sadece B, K, b ve k girebilirsiniz");            
+        }
+    }
+
+}
 
